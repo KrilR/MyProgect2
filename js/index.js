d57722bd7a1e38829e8b411e509ea60b7919bd35
Roksolana KRil <roksolana.kr@gmail.com>
2017-03-12 22:19:38 +0000

    add contact form, animation, tehnologies icons

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
 var i=10;
-setInterval(changeText,2000);
+setInterval(changeText,3000);
 function changeText(){
-    $("#target").fadeIn(1000);     
-    $("#target").fadeOut(1000);
+    $("#target").fadeIn(1500);     
+    $("#target").fadeOut(1500);
  var arr = ["safe","quick","style","fancy"];
     target.innerHTML=arr[i%4];
     i++;
-
 }
-
+   $("#questForm").submit(function(){
+      event.preventDefault();
+      $("#questForm").hide();
+      document.getElementById("thanks").hidden=false;
+    });
+$(function() {
+    new WOW().init();    
+});
 
